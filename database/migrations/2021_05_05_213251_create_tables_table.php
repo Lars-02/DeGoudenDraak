6377e4494555153b5f9fd3e0eed14d85472d21a5
Lars Meeuwsen <larsmeeuwsen@gmail.com>
2021-05-06 00:45:53 +0200

    Create Tables

---
+++ database/migrations/2021_05_05_213251_create_tables_table.php
@@ -0,0 +1,50 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTablesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tables', function (Blueprint $table) {
+            $table->id();
+            $table->integer('seats');
+        });
+
+        Schema::create('table_user', function (Blueprint $table) {
+            $table->primary(['table_id', 'user_id']);
+            $table->unsignedBigInteger('table_id');
+            $table->unsignedBigInteger('user_id');
+            $table->time('start_time');
+            $table->time('duration');
+            $table->integer('guests');
+            $table->boolean('help_request');
+
+            $table->foreign('table_id')
+                ->references('id')
+                ->on('tables')
+                ->cascadeOnDelete();
+            $table->foreign('user_id')
+                ->references('id')
+                ->on('users')
+                ->cascadeOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tables');
+    }
+}
