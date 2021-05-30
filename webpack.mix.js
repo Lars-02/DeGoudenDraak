cc72adceb770a879bc223ac4496e43ef54759941
Lars Meeuwsen <larsmeeuwsen@gmail.com>
2021-05-30 14:26:32 +0200

    Reimport VueJS

--- webpack.mix.js
+++ webpack.mix.js
@@ -15,5 +15,6 @@ mix.js('resources/js/app.js', 'public/js')
     .postCss("resources/css/app.css", "public/css", [
         require("tailwindcss"),
     ])
+    .vue()
     .sass('resources/sass/app.scss', 'public/css')
     .sourceMaps();
