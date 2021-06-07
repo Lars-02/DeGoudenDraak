f7f498b8c93919f534547a3e78ddcc1d85bdc192
Lars Meeuwsen <larsmeeuwsen@gmail.com>
2021-06-07 03:14:16 +0200

    Add Lang

---
+++ resources/lang/nl/serving.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+
+    'number' => 'Nummer',
+    'version' => 'Versie',
+    'category' => 'Categorie',
+    'categories' => 'Categoriën',
+    'name' => 'Naam',
+    'description' => 'Descriptie',
+    'spice' => 'Pittigheid',
+    'price' => 'Prijs',
+    'offer' => 'Korting',
+    'offers' => 'Kortingen',
+
+];
