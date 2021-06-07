6353cfa27bc7f32e2f093842977339594d150920
CheezyOP <vc.willemsen@student.avans.nl>
2021-06-07 03:00:27 +0200

    Added discount in pdf
    Did introduce a dirty fix

--- app/Http/Controllers/MenuController.php
+++ app/Http/Controllers/MenuController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Offer;
 use App\Models\Serving;
 use Carbon\Carbon;
 use Mpdf\Mpdf;
@@ -15,8 +16,10 @@ class MenuController extends Controller
         require_once(__ROOT__ . '\vendor\autoload.php');
 
         $servings = Serving::all();
+        $discounts = Offer::where('ending_at', '>=', Carbon::now())->get();
 
         $mpdf = new Mpdf();
+
         $mpdf->WriteHTML('<h2>De Gouden Draak</h2><h1>Menu</h1><h3>(' . Carbon::now() . ')</h3>');
         $mpdf->WriteHTML('<div class="container">
                 <div class="row justify-content-center">
@@ -31,24 +34,52 @@ class MenuController extends Controller
                         </tr>
                         </thead>
                         <tbody>');
-
-
         foreach ($servings as $serving) {
             $mpdf->WriteHTML('
                     <tr>
-                                    <th>' . $serving->id . '</th>
-                                    <th>' . $serving->number . '</th>
+                                    <td style="text-align:center">' . $serving->id . '</td>
+                                    <td style="text-align:center">' . $serving->number . '</td>
                                     <td>' . $serving->name . '</td>
                                     <td>' . $serving->description . '</td>
-                                    <td>' . $serving->spice . '</td>
+                                    <td style="text-align:center">' . $serving->spice . '</td>
                                 </tr>');
         }
-
         $mpdf->WriteHTML('                </tbody>
                     </table>
                 </div>
-            </div>');
-        $mpdf->Output('Menu.pdf', 'D');
+            </div> <pagebreak/>');
+
+        $mpdf->WriteHTML('<h2>De Gouden Draak</h2><h1>Aanbiedingen</h1><h3>(Geldig op: ' . Carbon::now() . ')</h3>');
+                $mpdf->WriteHTML('<div class="container">
+                        <div class="row justify-content-center">
+                            <table class="table-fixed bg-yellow-50">
+                                <thead>
+                                <tr>
+                                    <th class="w-1/8">ID</th>
+                                    <th class="w-1/8">Prijs</th>
+                                    <th class="w-3/8">Startdatum</th>
+                                    <th class="w-3/8">Einddatum</th>
+                                </tr>
+                                </thead>
+                                <tbody>');
+
+
+                foreach ($discounts as $discount) {
+                    $mpdf->WriteHTML('
+                            <tr>
+                                            <td style="text-align:center">' . $discount->id . '</td>
+                                            <td style="text-align:center">&#8364;' . $discount->price . '</td>
+                                            <td>' . $discount->start_at . '</td>
+                                            <td>' . $discount->ending_at . '</td>
+                                        </tr>');
+                }
+
+                $mpdf->WriteHTML('                </tbody>
+                            </table>
+                        </div>
+                    </div>');
+                $mpdf->Output('Menu.pdf', 'D');
+
 
         return redirect('menu');
     }
