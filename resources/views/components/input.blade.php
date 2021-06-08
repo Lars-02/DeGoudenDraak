47c4a7c3d53cd454413a34abc8fe6a56be81dc3b
Lars Meeuwsen <larsmeeuwsen@gmail.com>
2021-06-08 15:31:48 +0200

    Update input.blade.php

--- resources/views/components/input.blade.php
+++ resources/views/components/input.blade.php
@@ -1,12 +1,13 @@
-@props(['id', 'value' => ''])
+@props(['id', 'value' => '', 'type' => 'text'])
 
 <div class="select-none mb-4 xl:mb-6">
     <label class="select-none text-xs sm:text-sm md:text-base lg:text-lg mb-1.5 pl-1.5 py-0.5 float-left text-left text-white w-2/3 bg-gray-400 rounded font-medium" for="{{$id}}">{{$slot}}</label>
     <div class="relative">
         <input
-            id="{{$id}}"
-            placeholder="{{$slot}}..."
-            name="{{$id}}"
+            id="{{ $id }}"
+            name="{{ $id }}"
+            type="{{ $type }}"
+            placeholder="{{ $slot }}..."
             value="{{ old($id, $value) }}"
             {{$attributes->class(['select-text text-xs sm:text-sm md:text-base lg:text-lg px-2.5 py-2.5 w-full rounded border-gray-400 focus:border-gray-400 text-gray-600 focus:ring-0'])}}
         />
