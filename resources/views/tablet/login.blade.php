5655315df8338b33efaae3253d85f5a3c149103a
CheezyOP <vc.willemsen@student.avans.nl>
2021-06-16 17:59:02 +0200

    Temp fix

--- resources/views/tablet/login.blade.php
+++ resources/views/tablet/login.blade.php
@@ -40,7 +40,7 @@
             </div>
         </div>
 
-        <form action="/tablet/menu" method="POST">
+        <form action="/tablet/menu" method="GET">
             @csrf
             <div class="select-none grid gap-1 sm:gap-2 md:gap-4 grid-cols-1 sm:grid-cols-2 xl:grid-cols-3">
                 <x-input type="number" id="number" min="0">Tafelnummer</x-input>
