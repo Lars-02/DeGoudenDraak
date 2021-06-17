a0aa57888a0e1b53e6a0ad9ec67c9d8b258a3871
Lars Meeuwsen <larsmeeuwsen@gmail.com>
2021-06-17 12:32:47 +0200

    Add Login and Register

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Role;
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $admin = User::factory()
+            ->create([
+                'firstname' => 'Super',
+                'lastname' => 'Admin',
+                'email' => 'admin@admin.com']);
+        $admin->roles()->sync(Role::all()->where('name','Admin'));
+        User::factory()
+            ->create([
+                'firstname' => 'Lars',
+                'lastname' => 'Meeuwsen',
+                'email' => 'larsmeeuwsen@gmail.com']);
+        User::factory()
+            ->create([
+                'firstname' => 'Vincent',
+                'lastname' => 'Willemsen',
+                'email' => 'vc.willemsen@student.avans.nl']);
+    }
+}
