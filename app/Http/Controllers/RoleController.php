a49c4a1a40f27a76476a57a8deadccacd7e58f04
Lars Meeuwsen <larsmeeuwsen@gmail.com>
2021-06-20 14:32:54 +0200

    Add Role Edit and Update

--- app/Http/Controllers/RoleController.php
+++ app/Http/Controllers/RoleController.php
@@ -2,12 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\RoleRequest;
+use App\Models\Ability;
 use App\Models\Role;
 use App\Models\User;
 use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Contracts\View\Factory;
 use Illuminate\Contracts\View\View;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\Routing\Redirector;
+use Illuminate\Validation\ValidationException;
 
 class RoleController extends Controller
 {
@@ -35,11 +40,14 @@ class RoleController extends Controller
      * Show the form for editing the specified resource.
      *
      * @param Role $role
-     * @return void
+     * @return Application|Factory|View
      */
     public function edit(Role $role)
     {
-        //
+        $abilities = Ability::all()->mapWithKeys(function ($item) use ($role) {
+            return [$item['id'] => [$role->abilities->contains($item['id']), $item['name']]];
+        });
+        return view('role.edit', ['role' => $role, 'abilities' => $abilities]);
     }
 
     /**
@@ -47,10 +55,19 @@ class RoleController extends Controller
      *
      * @param Request $request
      * @param Role $role
-     * @return void
+     * @return Application|RedirectResponse|Redirector
+     * @throws ValidationException
      */
     public function update(Request $request, Role $role)
     {
-        //
+        $validated = $this->validate($request, [
+            'abilities' => 'nullable|array',
+            'abilities.*' => 'nullable|numeric|distinct|exists:abilities,id',
+        ]);
+        if (in_array(null, $validated['abilities']))
+            $role->abilities()->detach();
+        else
+            $role->abilities()->sync($validated['abilities']);
+        return redirect(route('role.index'));
     }
 }
